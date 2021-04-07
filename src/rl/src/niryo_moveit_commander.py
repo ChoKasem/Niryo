de52f0fdc597f70a7fa9dd8d2173f6e63fb79d12
Chokpisit K <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2021-04-07 16:39:14 +0700

    add niryo moveit commander to allow python interface control, also add them in Cmake and package

---
+++ src/rl/src/niryo_moveit_commander.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+
+class MoveGroupPythonIntefaceTutorial(object):
+  """MoveGroupPythonIntefaceTutorial"""
+  def __init__(self):
+    super(MoveGroupPythonIntefaceTutorial, self).__init__()
+
+    ## BEGIN_SUB_TUTORIAL setup
+    ##
+    ## First initialize `moveit_commander`_ and a `rospy`_ node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial',
+                    anonymous=True)
+
+    ## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
+    ## the robot:
+    robot = moveit_commander.RobotCommander()
+
+    ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
+    ## to the world surrounding the robot:
+    scene = moveit_commander.PlanningSceneInterface()
+
+    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+    ## to one group of joints.  In this case the group is the joints in the Panda
+    ## arm so we set ``group_name = panda_arm``. If you are using a different robot,
+    ## you should change this value to the name of your robot arm planning group.
+    ## This interface can be used to plan and execute motions on the Panda:
+    group_name = "arm"
+    group = moveit_commander.MoveGroupCommander(group_name)
+
+    ## We create a `DisplayTrajectory`_ publisher which is used later to publish
+    ## trajectories for RViz to visualize:
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+
+    ## END_SUB_TUTORIAL
+
+    ## BEGIN_SUB_TUTORIAL basic_info
+    ##
+    ## Getting Basic Information
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+    # We can get the name of the reference frame for this robot:
+    planning_frame = group.get_planning_frame()
+    print "============ Reference frame: %s" % planning_frame
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = group.get_end_effector_link()
+    print "============ End effector: %s" % eef_link
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print "============ Robot Groups:", robot.get_group_names()
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print "============ Printing robot state"
+    print robot.get_current_state()
+    print ""
+    ## END_SUB_TUTORIAL
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.group = group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
+  def go_to_pose_goal(self):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    group = self.group
+
+    ## BEGIN_SUB_TUTORIAL plan_to_pose
+    ##
+    ## Planning to a Pose Goal
+    ## ^^^^^^^^^^^^^^^^^^^^^^^
+    ## We can plan a motion for this group to a desired pose for the
+    ## end-effector:
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.x = 0.361772034518
+    pose_goal.orientation.y = 0.609889820653
+    pose_goal.orientation.z = 0.606442770113
+    pose_goal.orientation.w = 0.359697884734
+    pose_goal.position.x = 0.215643591815
+    pose_goal.position.y = 0.117823111836
+    pose_goal.position.z = 0.41643872883
+    group.set_pose_target(pose_goal)
+
+    ## Now, we call the planner to compute the plan and execute it.
+    plan = group.go(wait=True)
+    # Calling `stop()` ensures that there is no residual movement
+    group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    group.clear_pose_targets()
+
+    ## END_SUB_TUTORIAL
+
+    # For testing:
+    # Note that since this section of code will not be included in the tutorials
+    # we use the class variable rather than the copied state variable
+    current_pose = self.group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.01)    
+
+def main():
+    print "============ Press `Enter` to begin the tutorial by setting up the moveit_commander (press ctrl-d to exit) ..."
+    raw_input()
+    tutorial = MoveGroupPythonIntefaceTutorial()
+    print "============ Press `Enter` to execute a movement using a pose goal ..."
+    raw_input()
+    tutorial.go_to_pose_goal()
+    
+if __name__ == '__main__':
+    main()
+    print("Done")
