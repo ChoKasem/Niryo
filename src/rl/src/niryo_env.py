cc6d58ba817e87de4a2f2f18c80a4b09a39af5a5
Chokpisit K <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2021-05-07 23:55:37 +0700

    modify test

--- src/rl/src/niryo_env.py
+++ src/rl/src/niryo_env.py
@@ -4,6 +4,7 @@ import numpy as np
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState, Image
+from control_msgs.msg import GripperCommandActionGoal
 import niryo_moveit_commander
 
 # TODO have Niryo inherit moveit commander, or maybe super
@@ -12,12 +13,10 @@ class Niryo:
         rospy.loginfo("Initialize Niryo RL Node")
         rospy.init_node('Niryo_RL_Node',
                     anonymous=True)
-        self.state = Niryo_State()            
+        self.arm = Arm()            
         self.command = niryo_moveit_commander.MoveGroupPythonInteface("arm")
         
-        # gripper have move group, but don't have planning tool so cannot control
-        self.gripper_cmd = niryo_moveit_commander.MoveGroupPythonInteface("gripper")
-        self.gripper = Gripper_State()
+        self.gripper = Gripper()
     
     def go_to_pose(self, pos_x = 0, pos_y= 0, pos_z = 0, ori_x = 0, ori_y = 0, ori_z = 0, ori_w = 0):
         self.command.go_to_pose_goal(pos_x, pos_y, pos_z, ori_x, ori_y, ori_z, ori_w)
@@ -26,12 +25,16 @@ class Niryo:
         # starting joint state
         joints = [-4.00038318737e-05, -0.00169649498877, -0.00135103272703, 1.82992589703e-05, -0.0005746965517, 7.78535278902e-05]
         self.command.go_to_joint_state(joints)
-        
+        gripper.grab_angle(0)
+
+    def step(self, end_effector_pose, gripper_angle):
+        go_to_pose(end_effector_pose[0],end_effector_pose[1],end_effector_pose[2],end_effector_pose[3],end_effector_pose[4],end_effector_pose[5],end_effector_pose[6])
+        gripper.grab_angle(gripper_angle)
 
     def compute_reward(self):
         pass
 
-class Niryo_State:
+class Arm:
     def __init__(self):
         '''
         0 = joint_1
@@ -67,24 +70,35 @@ class Niryo_State:
     def joint_cb(self, msg):
         self.joint_angle = msg
         
-class Gripper_State:
-    '''
-    0 = tool_joint
-    1 = mainsupport_joint
-    2 = gripper_joint
-    3 = left_clamp_joint
-    4 = left_rod_joint
-    5 = motor_joint
-    6 = right_gear_joint
-    7 = right_clamp_joint
-    8 = right_rod_joint
-    '''
+class Gripper:
+    
+    def __init__(self):
+        '''
+        0 = tool_joint
+        1 = mainsupport_joint
+        2 = gripper_joint
+        3 = left_clamp_joint
+        4 = left_rod_joint
+        5 = motor_joint
+        6 = right_gear_joint
+        7 = right_clamp_joint
+        8 = right_rod_joint
+        '''
+        self.gripper_pub = rospy.Publisher('/gripper_controller/gripper_cmd/goal', GripperCommandActionGoal, queue_size=10)
+
+    def grab_angle(self, angle): #angle max at 1.2
+        gripperGoal = GripperCommandActionGoal()
+        gripperGoal.goal.command.position = angle
+        print(gripperGoal)
+        rospy.sleep(1)
+        self.gripper_pub.publish(gripperGoal)
+        
+
+class World:
     pass
 
-def test():
+def test_arm():
     niryo = Niryo()
-    tool_joint = [3.41760362232e-08, -9.71873266309e-07, 2.86864792365e-07, 0.0134953835178, -0.0164044312931, 0.0168274248518]
-    niryo.gripper_cmd.go_to_joint_state(tool_joint)
     niryo.command.go_to_pose_goal(0.350840341432, -0.058138712168, 0.276432223498, 0.50174247142, 0.501506407284, 0.498433947182, 0.498306548344)
     print("Press Enter")
     raw_input()
@@ -95,6 +109,13 @@ def test2():
     while True:
         print(niryo.state.joint_angle)
 
+def test_gripper():
+    niryo = Niryo()
+    niryo.gripper.grab_angle(1.2)
+    rospy.sleep(2)
+    niryo.gripper.grab_angle(0.3)
+
 if __name__ == '__main__':
-    test2()
+    test_arm()
+    # test_gripper()
     
